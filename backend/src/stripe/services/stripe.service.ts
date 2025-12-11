8c1a7250ed5b151090a429dcd0813980781e8e1d
alkisax <alkisax@gmail.com>
2025-12-11 15:48:23 +0200

    wip: bug in checkout success #no git actions -missing metadata

--- backend/src/stripe/services/stripe.service.ts
+++ backend/src/stripe/services/stripe.service.ts
@@ -4,16 +4,21 @@
 
 import Stripe from 'stripe';
 import { buildLineItems } from './stripe.functions.helper';
-import type { CartType, ParticipantType, ShippingInfoType, lineItemsType } from '../types/stripe.types';
+import type {
+  CartType,
+  ParticipantType,
+  ShippingInfoType,
+  lineItemsType,
+} from '../types/stripe.types';
 
 if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error ('missing env variables');
+  throw new Error('missing env variables');
 }
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // παίρνει απο το φροντ το cart και της πληροφορίες του πελάτη ως μεταντατα.
 const createCheckoutSession = async (
-  cart: CartType, 
+  cart: CartType,
   participantInfo: Partial<ParticipantType> = {},
   shippingInfo: Partial<ShippingInfoType> = {}
 ) => {
@@ -35,20 +40,29 @@ const createCheckoutSession = async (
     country: shippingInfo.country || '',
     phone: shippingInfo.phone || '',
     notes: shippingInfo.notes || '',
-    shippingMethod: (shippingInfo as ShippingInfoType).shippingMethod || 'pickup',
+    shippingMethod:
+      (shippingInfo as ShippingInfoType).shippingMethod || 'pickup',
   };
   console.log('Creating checkout session with metadata:', metadata);
 
-  const line_items: lineItemsType[] = buildLineItems(cart, shippingInfo.shippingMethod);
+  const line_items: lineItemsType[] = buildLineItems(
+    cart,
+    shippingInfo.shippingMethod
+  );
 
   //Stripe will still show  Google Pay / Revolut if you have them enabled in your dashboard.
+  console.log('🔥🔥🔥 METADATA SENT TO STRIPE:');
+  console.log(JSON.stringify(metadata, null, 2));
+
   return await stripe.checkout.sessions.create({
     mode: 'payment',
     payment_method_types: ['card'],
     line_items,
-    success_url: `${process.env.FRONTEND_URL || 'http://localhost:5173'}/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
+    success_url: `${
+      process.env.FRONTEND_URL || 'http://localhost:5173'
+    }/checkout-success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${FRONTEND_URL}/cancel?canceled=true`,
-    metadata: metadata
+    metadata: metadata,
   });
 };
 
@@ -58,5 +72,5 @@ const retrieveSession = async (sessionId: string) => {
 
 export const stripeService = {
   createCheckoutSession,
-  retrieveSession
+  retrieveSession,
 };
