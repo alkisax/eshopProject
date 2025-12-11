6374e7d73834524945a00038b04d89af09e5115e
alkisax <alkisax@gmail.com>
2025-12-11 11:58:41 +0200

    wip: bug in checkout success

--- backend/src/stripe/routes/stripe.routes.ts
+++ backend/src/stripe/routes/stripe.routes.ts
@@ -6,6 +6,7 @@ router.post('/checkout/cart', stripeController.createCheckoutSession);
 router.get('/cancel', stripeController.handleCancel);
 
 // router.get('/success', stripeController.handleSuccess);
+// είναι commeted out γιατί έπειδή είναι raw έχει μεταφερθεί στο app.ts
 // router.post( '/webhook', express.raw({ type: 'application/json' }), stripeController.handleWebhook);
 
 export default router;
