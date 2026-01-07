84c9623f0fc4d8c9d3629e74b7f52abce593a458
alkisax <alkisax@gmail.com>
2026-01-07 12:59:38 +0200

    fix: lazy load on non important or admin pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@
 
 import { Routes, Route } from "react-router-dom";
 import { useContext } from "react";
+import { Suspense, lazy } from "react";
 
 import { useInitializer } from "./hooks/useInitializer";
 import { VariablesContext } from "./context/VariablesContext";
@@ -10,7 +11,6 @@ import { UserAuthContext } from "./context/UserAuthContext";
 import Home from "./pages/Home";
 import Login from "./authLogin/Login";
 import GoogleLogin from "./authLogin/loginGoogle/GoogleLogin";
-import AdminPanel from "./pages/AdminPanel";
 import AdminPrivateRoute from "./authLogin/service/AdminPrivateRoute";
 import RegisterPageBackend from "./authLogin/loginBackend/RegisterPageBackend";
 import GithubSuccess from "./authLogin/loginGithub/GithubSuccess";
@@ -19,27 +19,13 @@ import RegisterPageAppwriteLogin from "./authLogin/loginAppwrite/RegisterPageApp
 import HomeAppwriteLogin from "./authLogin/service/Protected";
 import PrivateRoute from "./authLogin/service/PrivateRoute";
 import LayoutWithNavbar from "./Layouts/LayoutWithNavbar";
-import AdminLayout from "./Layouts/AdminLayout";
 import ProfileUser from "./pages/ProfileUser";
-import Store from "./pages/Store";
 import Cart from "./pages/Cart";
 import FavoritesPage from "./pages/FavoritesPage";
 import ShippingInfo from "./pages/ShippingInfo";
 import CheckoutSuccess from "./components/store_components/CheckoutSuccess";
-import CommodityPage from "./components/store_components/CommodityPage";
 import StoreLayout from "./Layouts/StoreLayout";
-import AdminAddNewCommodity from "./components/store_components/adminPannelCommodity/AdminAddNewCommodity";
 import Announcements from "./blog/blogPages/Announcements";
-import News from "./blog/blogPages/News";
-import BlogPost from "./blog/blogPages/BlogPost";
-import Contact from "./pages/minorPages/Contact";
-import Terms from "./pages/minorPages/Terms";
-import PrivacyPolicy from "./pages/minorPages/PrivacyPolicy";
-import PaymentMethods from "./pages/minorPages/PaymentMethods";
-import ShippingMethods from "./pages/minorPages/ShippingMethods";
-import ReturnPolicy from "./pages/minorPages/ReturnPolicy";
-import CookiePolicy from "./pages/minorPages/CookiePolicy";
-
 import GAAnalyticsTracker from "./utils/GAAnalyticsTracker";
 import LayoutWithNavbarAndFooter from "./Layouts/LayoutWithNavbarAndFooter";
 import { CartActionsContext } from "./context/CartActionsContext";
@@ -47,6 +33,31 @@ import HomeResponsiveWrapper from "./Layouts/HomeResponsiveWrapper";
 import CrossGridLayout from "./Layouts/deisgnComponents/CrossGridLayout";
 import AboutPage from "./pages/minorPages/AboutPage";
 import { useSettings } from "./context/SettingsContext";
+// lazy loads  + suspense
+const Store = lazy(() => import("./pages/Store"));
+const CommodityPage = lazy(
+  () => import("./components/store_components/CommodityPage")
+);
+const BlogPost = lazy(() => import("./blog/blogPages/BlogPost"));
+const News = lazy(() => import("./blog/blogPages/News"));
+const Contact = lazy(() => import("./pages/minorPages/Contact"));
+const Terms = lazy(() => import("./pages/minorPages/Terms"));
+const PrivacyPolicy = lazy(() => import("./pages/minorPages/PrivacyPolicy"));
+const PaymentMethods = lazy(() => import("./pages/minorPages/PaymentMethods"));
+const ShippingMethods = lazy(
+  () => import("./pages/minorPages/ShippingMethods")
+);
+const ReturnPolicy = lazy(() => import("./pages/minorPages/ReturnPolicy"));
+const CookiePolicy = lazy(() => import("./pages/minorPages/CookiePolicy"));
+// δεν θέλουμε να φορτώνουν οι σελίδες του admin σε όλους για λόγους οικονομίας. Και για αυτό κάναμε όλα τα σχετιά components lazy load και εδώ προσθέσσαμε Suspense. Tο suspense είναι μέσα στο admin private route μεα αποτέλεσμα να μην φορτώνετε καθόλου αν δεν είναι dmin
+const AdminPanel = lazy(() => import("./pages/AdminPanel"));
+const AdminLayout = lazy(() => import("./Layouts/AdminLayout"));
+const AdminAddNewCommodity = lazy(
+  () =>
+    import(
+      "./components/store_components/adminPannelCommodity/AdminAddNewCommodity"
+    )
+);
 
 function App() {
   const { user } = useContext(UserAuthContext);
@@ -90,21 +101,139 @@ function App() {
               )
             }
           />
-          <Route path="/news" element={<News />} />
-          <Route path="/announcements" element={<Announcements />} />
-          <Route path="/posts/:slug" element={<BlogPost />} />
 
           {/* used layout as origin of logic for store because its the component that encapsulates al the components. but this propably is not right because thats not the "layout" function. but will leave it as is */}
           {/* layout works together with sidebar and item list */}
+          {/* STORE */}
           <Route element={<StoreLayout />}>
-            <Route path="/store" element={<Store />} />
+            <Route
+              path="/store"
+              element={
+                <Suspense fallback={null}>
+                  <Store />
+                </Suspense>
+              }
+            />
           </Route>
+          <Route
+            path="/commodity/:slugOrId"
+            element={
+              <Suspense fallback={null}>
+                <CommodityPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/news"
+            element={
+              <Suspense fallback={null}>
+                <News />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/announcements"
+            element={
+              <Suspense fallback={null}>
+                <Announcements />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/posts/:slug"
+            element={
+              <Suspense fallback={null}>
+                <BlogPost />
+              </Suspense>
+            }
+          />
+          {/* Static / Minor Pages — all wrapped in CrossGridLayout */}
+          <Route
+            path="/contact"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Επικοινωνία">
+                  <Contact />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/privacy-policy"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Πολιτική Απορρήτου">
+                  <PrivacyPolicy />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/payment-methods"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Τρόποι Πληρωμής">
+                  <PaymentMethods />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/shipping-methods"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Τρόποι Αποστολής">
+                  <ShippingMethods />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/return-policy"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Πολιτική Επιστροφών">
+                  <ReturnPolicy />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/cookie-policy"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Πολιτική Cookies">
+                  <CookiePolicy />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
 
-          <Route path="/commodity/:slugOrId" element={<CommodityPage />} />
+          <Route
+            path="/about"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Σχετικά με Εμάς">
+                  <AboutPage />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+          <Route
+            path="/terms"
+            element={
+              <Suspense fallback={null}>
+                <CrossGridLayout title="Όροι Χρήσης">
+                  <Terms />
+                </CrossGridLayout>
+              </Suspense>
+            }
+          />
+
+          {/* NON-LAZY */}
           <Route path="/cart" element={<Cart />} />
           <Route path="/shipping-info" element={<ShippingInfo />} />
           <Route path="/favorites" element={<FavoritesPage />} />
-
           <Route path="/login" element={<Login url={url} />} />
           <Route path="/signup" element={<GoogleLogin url={url} />} />
           <Route
@@ -124,89 +253,28 @@ function App() {
             <Route path="/protected" element={<HomeAppwriteLogin />} />
             <Route path="/profile" element={<ProfileUser />} />
           </Route>
-
-          {/* Static / Minor Pages — all wrapped in CrossGridLayout */}
-          <Route
-            path="/contact"
-            element={
-              <CrossGridLayout title="Επικοινωνία">
-                <Contact />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/privacy-policy"
-            element={
-              <CrossGridLayout title="Πολιτική Απορρήτου">
-                <PrivacyPolicy />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/payment-methods"
-            element={
-              <CrossGridLayout title="Τρόποι Πληρωμής">
-                <PaymentMethods />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/shipping-methods"
-            element={
-              <CrossGridLayout title="Τρόποι Αποστολής">
-                <ShippingMethods />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/return-policy"
-            element={
-              <CrossGridLayout title="Πολιτική Επιστροφών">
-                <ReturnPolicy />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/cookie-policy"
-            element={
-              <CrossGridLayout title="Πολιτική Cookies">
-                <CookiePolicy />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/about"
-            element={
-              <CrossGridLayout title="Σχετικά με Εμάς">
-                <AboutPage />
-              </CrossGridLayout>
-            }
-          />
-
-          <Route
-            path="/terms"
-            element={
-              <CrossGridLayout title="Όροι Χρήσης">
-                <Terms />
-              </CrossGridLayout>
-            }
-          />
         </Route>
 
         {/* Admin routes → Navbar only, no Footer */}
+        {/* δεν θέλουμε να φορτώνουν οι σελίδες του admin σε όλους για λόγους οικονομίας. Και για αυτό κάναμε όλα τα σχετιά components lazy load και εδώ προσθέσσαμε Suspense */}
         <Route element={<AdminPrivateRoute />}>
           <Route element={<LayoutWithNavbar />}>
             <Route element={<AdminLayout />}>
-              <Route path="/admin-panel" element={<AdminPanel />} />
+              <Route
+                path="/admin-panel"
+                element={
+                  <Suspense fallback={null}>
+                    <AdminPanel />
+                  </Suspense>
+                }
+              />
               <Route
                 path="/admin-panel/commodity/new"
-                element={<AdminAddNewCommodity />}
+                element={
+                  <Suspense fallback={null}>
+                    <AdminAddNewCommodity />
+                  </Suspense>
+                }
               />
             </Route>
           </Route>
